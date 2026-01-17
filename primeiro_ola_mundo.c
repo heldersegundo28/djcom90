3546181ec442a56839e3746aa055e0b29a294b70
heldersegundo28 <heldersegundo28@gmail.com>
2026-01-17 19:20:47 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .vscode/launch.json
    	new file:   .vscode/tasks.json
    	new file:   primeiro_ola_mundo
    	new file:   primeiro_ola_mundo.c
    	new file:   teste

---
+++ primeiro_ola_mundo.c
@@ -0,0 +1,73 @@
+// # include <stdio.h>
+// int main () {
+//      printf ("ola mundo");
+
+
+
+//     return 0;
+// }      // aqui eu consegui fazer o primeiro ola mundo e eu estava escrevendo erro no stdio e eu escrevia studio.h kkkk//
+
+
+
+// #include <stdio.h>
+ 
+// int main() {
+//     int idade = 25;
+//     float altura = 1.75;
+//     double saldoBancario = 12345.67;
+//     char inicial = 'A';
+//     char nome[20] = "Bruno";
+ 
+//     printf("Idade: %d anos\n", idade);
+//     printf("Altura: %.2f metros\n", altura);
+//     printf("Saldo Bancário: %.2f reais\n", saldoBancario);
+//     printf("Inicial do Nome: %c\n", inicial);
+//     printf("Nome: %s\n", nome);
+ 
+//     return 0;
+// }  // aqui ja tivemos minha evolução 
+
+// #include <stdio.h>
+ 
+// int main() {
+//     int idade;
+//     printf("Digite sua idade: \n");
+//     scanf("%d", &idade);
+//     printf("Sua idade é: %d\n", idade);
+//     return 0;
+// } // e aqui ja fomos para o uso do scanf
+
+// #include <stdio.h>
+
+// int main() {
+//     char nome[50];
+//     int idade;
+//     float nota1, nota2, media;
+
+//     // Entrada de dados
+//     printf("Digite o nome do aluno: ");
+//     scanf(" %[^\n]", nome);  // lê texto com espaços
+
+//     printf("Digite a idade do aluno: ");
+//     scanf("%d", &idade);
+
+//     printf("Digite a primeira nota: ");
+//     scanf("%f", &nota1);
+
+//     printf("Digite a segunda nota: ");
+//     scanf("%f", &nota2);
+
+//     // Operação matemática
+//     media = (nota1 + nota2) / 2;
+
+//     // Saída de dados
+//     printf("\n--- DADOS DO ALUNO ---\n");
+//     printf("Nome: %s\n", nome);
+//     printf("Idade: %d anos\n", idade);
+//     printf("Nota 1: %.2f\n", nota1);
+//     printf("Nota 2: %.2f\n", nota2);
+//     printf("Media: %.2f\n", media);
+
+//     return 0;
+// } aqui ja fizemos um primeiro programa com interação só para testar
+
