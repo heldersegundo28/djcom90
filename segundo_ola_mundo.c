df7641bf96956c64a0b19cda72da048d911ec4e1
heldersegundo28 <heldersegundo28@gmail.com>
2026-01-23 12:24:23 +0000

    Changes to be committed: 	new file:   segundo_ola_mundo 	new file:   segundo_ola_mundo.c

---
+++ segundo_ola_mundo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+
+    int  num1;
+    int num2;
+    printf("digite o primeiro numero: ");
+    scanf("%d", &num1);
+    printf("digite o segundo numero: ");
+    scanf("%d", &num2);
+    if (num1 > num2)
+    {
+        printf("num1 é maior que num2\n");
+    }
+    else if (num1 < num2) {
+        printf("num2 é maior que num1\n");
+    } 
+    else {
+        printf("os numeros sao iguais\n");
+    }
+    
+    return 0;
+}
