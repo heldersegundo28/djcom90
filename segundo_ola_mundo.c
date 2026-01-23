ad347cfc5cd6b92caf20b91daaa84930d2a860c1
heldersegundo28 <heldersegundo28@gmail.com>
2026-01-23 13:02:40 +0000

    On branch main

--- segundo_ola_mundo.c
+++ segundo_ola_mundo.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    int  num1;
-    int num2;
-    printf("digite o primeiro numero: ");
+    int  num1; int resultado; 
+    printf("digite um numero: ");
     scanf("%d", &num1);
-    printf("digite o segundo numero: ");
-    scanf("%d", &num2);
-    if (num1 > num2)
+    resultado = num1 % 2;
+    printf("o resto da divisao de %d por 2 e: %d\n", num1, resultado);
+    if (resultado == 0)
     {
-        printf("num1 é maior que num2\n");
+        printf("o numero %d e par\n", num1);
     }
-    else if (num1 < num2) {
-        printf("num2 é maior que num1\n");
-    } 
-    else {
-        printf("os numeros sao iguais\n");
+    else
+    {
+        printf("o numero %d e impar\n", num1);
     }
-    
     return 0;
 } // estou aprendendo C, no caso hoje esta sendo scanf, if, else, else if, operadores relacionais e logicos.
